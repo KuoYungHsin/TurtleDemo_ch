d90f214da725566fd4e1dcd57f162caffbdb0926
renyuanLyu <renyuan.lyu@gmail.com>
2014-06-06 00:02:06 +0800

    ryModify
    self --> 我
    writer --> 寫字龜

--- k_colormixer.py
+++ k_colormixer.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+﻿# -*- coding: utf-8 -*-
 # colormixer
 
 
@@ -8,31 +8,31 @@ sys.setrecursionlimit(20000)   # overcomes, for now, an instability of Python 3.
 
 class 顏色龜(龜類):
 
-    def __init__(self, x, y):
-        龜類.__init__(self)
-        self.形狀(龜形)
-        self.重設大小模式("user")
-        self.形狀大小(3,3,5)
-        self.筆粗(10)
-        self._color = [0,0,0]
-        self.x = x
-        self._color[x] = y
-        self.顏色(self._color)
-        self.速度(0)
-        self.左轉(90)
-        self.提筆()
-        self.前往(x,0)
-        self.下筆()
-        self.設y座標(1)
-        self.提筆()
-        self.設y座標(y)
-        self.筆色("gray25")
-        self.在拖曳時(self.移動)
+    def __init__(我, x, y):
+        龜類.__init__(我)
+        我.形狀(龜形)
+        我.重設大小模式("user")
+        我.形狀大小(3,3,5)
+        我.筆粗(10)
+        我._color = [0,0,0]
+        我.x = x
+        我._color[x] = y
+        我.顏色(我._color)
+        我.速度(0)
+        我.左轉(90)
+        我.提筆()
+        我.前往(x,0)
+        我.下筆()
+        我.設y座標(1)
+        我.提筆()
+        我.設y座標(y)
+        我.筆色("gray25")
+        我.在拖曳時(我.移動)
 
-    def 移動(self, x, y):
-        self.設y座標(max(0,min(y,1)))
-        self._color[self.x] = self.y座標()
-        self.填色(self._color)
+    def 移動(我, x, y):
+        我.設y座標(max(0,min(y,1)))
+        我._color[我.x] = 我.y座標()
+        我.填色(我._color)
         設背景顏色()
 
 def 設背景顏色():
@@ -49,11 +49,11 @@ def main():
     藍龜 = 顏色龜(2, .5)
     設背景顏色()
 
-    writer = 龜類()
-    writer.藏龜()
-    writer.提筆()
-    writer.前往(1,1.15)
-    writer.寫("拖拉(DRAG)!",align="center",font=("Arial",30,("bold","italic")))
+    寫字龜 = 龜類()
+    寫字龜.藏龜()
+    寫字龜.提筆()
+    寫字龜.前往(1,1.15)
+    寫字龜.寫("拖拉(DRAG)!",align="center",font=("Arial",30,("bold","italic")))
     return "事件迴圈"
 
 if __name__ == "__main__":
